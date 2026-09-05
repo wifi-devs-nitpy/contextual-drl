c2931cf2208851f61ff14920c720c47cb8d8dcbe
devarshi-balu <studious.devarshibalu@gmail.com>
2026-09-05 11:05:12 +0530

    pin python version, extend the script with primlimanary runs

--- tests/runs_script.sh
+++ tests/runs_script.sh
@@ -1,3 +1,10 @@
 export PYTHONUNBUFFERED=true
 
-python run_experiment.py --d-ap 10 --d-sta 2 --n-runs 10 --n-steps 5000
+echo "Script execution started at - $(date) -"
+
+python run_experiment.py --d-ap 10 --d-sta 2 --n-runs 50 --n-steps 10000
+python run_experiment.py --d-ap 20 --d-sta 2 --n-runs 50 --n-steps 10000
+python run_experiment.py --d-ap 30 --d-sta 2 --n-runs 50 --n-steps 10000
+python run_experiment.py --d-ap 10 --d-sta 4 --n-runs 50 --n-steps 10000
+
+echo "Script execution completed at - $(date) -"
